e97cf7762693c3d975beedb7ba582080b3d54440
Thickduck <89892224+Thickduck@users.noreply.github.com>
2022-02-08 20:22:31 +0530

    added some functionality

--- index.js
+++ index.js
@@ -7,10 +7,12 @@ const unhide = (id) => {
 
 hide("input-tag");
 hide("submit-button");
+hide("clear-all");
 
 const add = () => {
   unhide("input-tag");
   unhide("submit-button");
+  unhide("clear-all");
 };
 
 let add_button = document.getElementById("add-button");
@@ -22,16 +24,35 @@ let input = document.getElementById("input-tag");
 let task_li = document.getElementById("task-list");
 
 const add_task = (inputField, list) => {
-  let value = inputField.value;
-  let entry = document.createElement("li");
-  entry.id = "task-item";
-  entry.appendChild(document.createTextNode(value));
-  list.appendChild(entry);
+  if (inputField.value.length > 0) {
+    let value = inputField.value;
+    let entry = document.createElement("li");
+    entry.className = "task-item";
+    entry.appendChild(document.createTextNode(value));
+    list.appendChild(entry);
 
-  inputField.value = "";
+    inputField.value = "";
+  }
 };
 
 let sub_button = document.getElementById("submit-button");
 sub_button.onclick = () => {
   add_task(input, task_li);
+};
+
+const clear_task = (list) => {
+  list.innerHTML = "";
+};
+let clear_button = document.getElementById("clear-all");
+clear_button.onclick = () => {
+  clear_task(task_li);
+};
+
+const remove_task = (list) => {
+  let items = list.getElementsByTagName("li");
+  for (let i = 0; i < items.length; i++) {
+    items[i].onclick = () => {
+      items[i].parentNode.removeChild(items[i]);
+    };
+  }
 };
